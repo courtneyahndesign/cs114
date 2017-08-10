605224881586ec709b1dc3dd7a9201221372c38b
Courtney Ahn <CourtneyAhn@Courtneys-MacBook-Pro.local>
2017-08-10 13:47:01 -0700

    updates

--- practice/vampire.py
+++ practice/vampire.py
@@ -1,10 +1,10 @@
 print("Hey, what's your name?")
 name = input()
 print("How old are you?")
-age = input()
-if int(age) < 90:
+age = int(input())
+if age < 90:
     print("* sigh * " + str(name) + ", I thought you were a vampire for a second there! Thank god!")
-elif int(age) > 90:
+elif age > 90:
     print( str(name) + ", you really shouldn't make it so obvious! It's okay, vampires are welcome here!")
 else:
     print("nevermind")
