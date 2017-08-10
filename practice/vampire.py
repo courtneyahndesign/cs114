fa2329c9650acb9d1316f13eeb84c9f501531c45
Courtney Ahn <CourtneyAhn@Courtneys-MacBook-Pro.local>
2017-08-10 13:41:08 -0700

    updates

---
+++ practice/vampire.py
@@ -0,0 +1,8 @@
+print("How old are you?")
+age = input()
+if int(age) < 90:
+    print("* sigh * I thought you were a vampire for a second there! Thank god!")
+elif int(age) > 90:
+    print("You really shouldn't make it so obvious! It's okay, vampires are welcome here!")
+else:
+    print("nevermind")
