d92aba4d9cbe943486646c2866c270257e7b2084
Courtney Ahn <CourtneyAhn@Courtneys-MacBook-Pro.local>
2017-08-10 15:07:14 -0700

    updates

--- practice/namerandom.py
+++ practice/namerandom.py
@@ -2,10 +2,15 @@
 
 print("What's your name?")
 name = input()
+length = len(name)
+
 import random
 for i in range(1):
     amount = (random.randint(1, 10))
+
+newamount = int(amount) + int(length)
+
 eggs = 0
-while eggs < int(amount):
+while eggs < int(newamount):
     print(name)
     eggs = eggs + 1
